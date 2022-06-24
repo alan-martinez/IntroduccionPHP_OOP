39b247dd01ff26eb1959f9b2a8dac75bd3653c8e
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-06-24 15:35:06 -0500

    Basicos POD con PHP 1

--- 01.php
+++ 01.php
@@ -2,20 +2,34 @@
 declare (strict_types = 1);
 include 'includes/header.php';
 
-//Definir una clas
+//Definir una clase
 class Producto {
-    public $nombre;
-    public $precio;
-    public $disponible;
+    /* MANERA VIEJA */
+    // public $nombre;
+    // public $precio;
+    // public $disponible;
 
-    public function __construct(string $nombre, int $precio, bool $disponible){
-        $this->nombre = $nombre;
-        $this->precio = $precio;
-        $this->disponible = $disponible;
+    // public function __construct(string $nombre, int $precio, bool $disponible){
+    //     $this->nombre = $nombre;
+    //     $this->precio = $precio;
+    //     $this->disponible = $disponible;
+    // }
+
+    public function __construct(public string $nombre, public int $precio, public bool $disponible){
+    
+    }
+
+    public function mostrarProducto(){
+        echo 'El producto es: ' . $this->nombre . ' y su precio: ' . $this->precio;
     }
 }
 
 $producto = new Producto('Tablet', 500, true);
+$producto->mostrarProducto();
+
+$producto2 = new Producto('Celular', 200, true);
+$producto2->mostrarProducto();
+
 // $producto->nombre = 'Tablet';
 // $producto->precio = 200;
 // $producto->disponible = true;
