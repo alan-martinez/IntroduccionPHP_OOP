9338aa5652dfa9f0bc08895c69e86323a211755f
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-06-30 10:09:43 -0500

    Final

--- 09.php
+++ 09.php
@@ -1,4 +1,32 @@
 <?php include 'includes/header.php';
 
+//Conectar a la DB con Mysql
+
+$db = new mysqli ('localhost', 'root', 'root', 'bienes_raices');
+
+//Creamos el query
+$query = ' SELECT titulo FROM propiedades';
+
+//Lo preparamos
+$stmt = $db->prepare($query);
+
+//Lo ejecutamos
+$stmt->execute();
+
+//Creamos la variable
+$stmt->bind_result($titulo);
+
+//Asignamos el resultado
+while($stmt->fetch()){
+    var_dump($titulo);
+}
+
+// var_dump($titulo);
+
+// while ($row = $resultado->fetch_assoc()):
+//     var_dump($row);
+// endwhile;
+
+// var_dump($resultado->fetch_assoc());
 
 include 'includes/footer.php';
--- 10.php
+++ 10.php
@@ -1,4 +1,12 @@
 <?php include 'includes/header.php';
 
+//Conectar a la BD con PDO
+$db = new PDO('mysql:host=localhost; dbname=bienes_raices', 'root', 'root');
+
+$query = ' SELECT titulo FROM propiedades';
+
+//Consultar la BD 
+$resultado = $db->query($query)->fetch();
+var_dump($resultado);
 
 include 'includes/footer.php';
